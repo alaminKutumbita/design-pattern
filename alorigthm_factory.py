a677f382b654f67ff990e9311cf256c8a3096c3e
Abdullah AL Amin <alamin@kutumbita.com>
2019-01-29 09:57:53 +0600

    globals used

--- alorigthm_factory.py
+++ alorigthm_factory.py
@@ -1,12 +1,7 @@
-from strategies import LocalGatewayAlgorithm, TwilioAlgorithm
-
-AlgorithmMap = {
-    'local': LocalGatewayAlgorithm(),
-    'twilio': TwilioAlgorithm()
-}
-
 
 class AlgorithmFactory(object):
 
-    def create_algorithm(self, name):
-        return AlgorithmMap[name]
+    @staticmethod
+    def create_algorithm(name):
+        target_class = name[:1].upper()+name[1:]+'Algorithm'
+        return globals()[target_class]()
--- app.py
+++ app.py
@@ -3,15 +3,14 @@ number = input('please enter number(with country code): ')
 message = input('type your message: ')
 
 
-def select_strategy(number):
-    if number.startswith('+880'):
-        return 'local'
+def select_strategy(num):
+    if num.startswith('+880'):
+        return 'localGateway'
     else :
         return 'twilio'
 
 
 strategy = select_strategy(number)
-
 
 message_sender = MessagingStrategy(strategy = strategy)
 message_sender.send_sms(message, number)
